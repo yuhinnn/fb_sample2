ac741c9cc0072ed0797605e5435690f7e8e51f84
yuhi_deve <yuhinnn@gmail.com>
2016-05-05 19:53:10 +0900

    add css index2.php

---
+++ index2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/index1.css">
+	<title>TBFBappresalt</title>
+</head>
+<body>
+	<div class="main">
+		<div class="content">
+			<div class="TBlogo">
+
+			</div>
+			<div class="textbox">
+				<h2>あなたの診断結果は・・・<br></h2>
+
+			</div>
+			<div class="pushbutton2">
+				<a href="#"><p class="pushtext2">いいねをする</p></a>
+			</div>
+			<div class="pushbutton3">
+				<a href="#"><p class="pushtext3">結果をシェアする</p></a>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
