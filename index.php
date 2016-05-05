d26949dfa3f2125698a49e2994bbbb1610dee8ee
yuhi_deve <yuhinnn@gmail.com>
2016-05-05 20:02:43 +0900

    add images renamed:index2.html -> index2.php

--- index.php
+++ index.php
@@ -4,25 +4,24 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/index1.css">
 	<title>TBFBapp</title>
- 
+
 </head>
 
 <body>
 	<div class="main">
 		<div class="content">
 			<div class="TBlogo">
-				
+
 			</div>
 			<div class="textbox">
 				<h2>あなたにぴったりな<br>旅先を診断してみよう！</h2>
-				
+
 			</div>
 			<div class="pushbutton">
-				<a href="index2.html"><p class="pushtext">診断スタート</p></a>
+				<a href="index2.php"><p class="pushtext">診断スタート</p></a>
 			</div>
-		</div>	
+		</div>
 	</div>
 
 </body>
 </html>
-
